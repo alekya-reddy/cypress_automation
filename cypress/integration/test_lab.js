2813fae4c36bf82b5d2f3ed136eef965f4ddb7bc
Liming Choi <liming.choi@gmail.com>
2020-09-18 22:18:14 -0400

    improved containsExact command to be able to handle all special characters - except for single quation but that is a cypress bug

--- cypress/integration/test_lab.js
+++ cypress/integration/test_lab.js
@@ -225,7 +225,32 @@ describe("Testing lab - Use this spec file to test out new techniques, or to hel
     })*/
 
     it("should always pass", ()=>{
-        cy.visit("https://www.google.com")
+        //cy.visit("https://www.google.com")
+        let name = `Rocket-Chat: Configurations, long_(name) with. special! character$ as "this" caused bug & stuff / yeah? 1234 #iamtheone++==*@`
+
+        function escapeNonAlphanumeric(text){
+            let charArray = text.split("")
+            let escapedCharArray = charArray.map((char)=>{
+                if(/[^a-zA-Z0-9\s]/.test(char)){
+                    return `\\${char}`
+                } else {
+                    return char
+                }
+            })
+            let escapedText = ""
+            escapedCharArray.forEach((char)=>{
+                escapedText = escapedText.concat(char)
+            })
+            return escapedText
+        }
+
+        let escapedName = escapeNonAlphanumeric(name)
+        cy.log(escapedName)
+
+        let text_regex = new RegExp(`^${escapedName}$`)
+        cy.log(text_regex)
+        expect(text_regex.test(name)).to.be.true 
+
     })
 
 })
