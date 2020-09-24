769e89fcd5d77b9d517d3cb2a61e3a52b49f2d7b
Liming Choi <liming.choi@gmail.com>
2020-09-24 17:04:56 -0400

    updated old env variables in spec files

--- cypress/integration/vex/webex.js
+++ cypress/integration/vex/webex.js
@@ -35,23 +35,23 @@ const session = {
 
 const contentSource = {
     'Image - Used by Cypress automation for VEX testing' : (function (server){
-        if(server == 'pathfactory-staging'){
+        if(server == authoring.common.env.envList.staging){
             return 'https://cdn.pathfactory-staging.com/assets/74/contents/12954/d8058125-c1a5-41d3-9f66-cf8176fe2040.png';
-        } else if (server == 'pathfactory-qa'){
+        } else if (server == authoring.common.env.envList.qa){
             return 'https://cdn.pathfactory-qa.com/assets/122/contents/17527/7e237afe-3aac-4d8b-bbeb-1a297fcd6fba.png';
-        } else if (server == 'prod'){
+        } else if (server == authoring.common.env.envList.prod){
             return 'https://cdn.pathfactory.com/assets/10668/contents/181009/31cacbc2-e385-4a58-881e-8fb175dbfa5b.png';
         }
-    })(Cypress.env("TEST_ENV")), 
+    })(authoring.common.env.TEST_ENV), 
     'PDF - Used by Cypress automation for VEX testing' : (function (server){
-        if(server == 'pathfactory-staging'){
+        if(server == authoring.common.env.envList.staging){
             return 'https://cdn.pathfactory-staging.com/assets/74/contents/12955/ae090cdf-c888-41f1-9c5c-5f20fee9acce.pdf';
-        } else if (server == 'pathfactory-qa'){
+        } else if (server == authoring.common.env.envList.qa){
             return 'https://cdn.pathfactory-qa.com/assets/122/contents/17526/6521cdc6-5677-493b-8040-3b0c3178a74e.pdf';
-        } else if (server == 'prod'){
+        } else if (server == authoring.common.env.envList.prod){
             return 'https://cdn.pathfactory.com/assets/10668/contents/181010/26a87f6a-9067-4247-b2ef-30764379b980.pdf';
         }
-    })(Cypress.env("TEST_ENV")),
+    })(authoring.common.env.TEST_ENV),
     'Website - Used by Cypress automation for VEX testing' : 'https://en.wikipedia.org/wiki/SpaceX'
 }
 
