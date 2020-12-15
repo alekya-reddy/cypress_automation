bd54087d66410397c90cef531905c5f1de4122f6
Liming Choi <liming.choi@gmail.com>
2020-12-15 14:22:13 -0500

    added test file for deleted microsites

---
+++ cypress/integration/microsites/deletedMicrosite.js
@@ -0,0 +1,52 @@
+import { createAuthoringInstance } from '../../support/pageObject.js'
+
+const authoring = createAuthoringInstance({org: "automation-microsites", tld: "lookbookhq"})
+
+const microsite = {
+    name: "deletedMicrosite.js",
+    slug: "deletedmicrosite-js",
+    get url(){
+        return `${authoring.common.baseUrl}/${this.slug}`
+    }
+}
+
+const landingPage = {
+    name: "Delete me",
+    slug: "delete-me",
+    get url(){
+        return `${microsite.url}/${this.slug}`
+    }
+}
+
+describe("Microsites - Deleted microsites and landing pages", () => {
+    it("A deleted landing page or microsite should no longer be accessible on consumption side", () => {
+        authoring.common.login()
+
+        // Setup
+        authoring.microsites.removeMicrosite(microsite.name)
+        authoring.microsites.addMicrosite(microsite.name)
+        authoring.microsites.setup(microsite)
+        authoring.microsites.addLandingPages(landingPage.name)
+
+        // Verify urls return a good response
+        cy.request({url: microsite.url, failOnStatusCode: false}).then((response)=>{
+            expect(response.status).to.eq(200)
+        })
+        cy.request({url: landingPage.url, failOnStatusCode: false}).then((response)=>{
+            expect(response.status).to.eq(200)
+        })
+
+        // Verify deleted landing page is no longer accessible
+        authoring.microsites.removeLandingPages(landingPage.name)
+        cy.request({url: landingPage.url, failOnStatusCode: false}).then((response)=>{
+            expect(response.status).to.eq(404)
+        })
+
+        // Verify deleted microsite is no longer accessible
+        authoring.microsites.visit()
+        authoring.microsites.removeMicrosite(microsite.name)
+        cy.request({url: microsite.url, failOnStatusCode: false}).then((response)=>{
+            expect(response.status).to.eq(404)
+        })
+    })
+})
