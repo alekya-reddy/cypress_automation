9c49fd7e49072021c778e68e97a666cb47a0332b
Mahender Goud Nadikude <mahender.nadikude@senecaglobal.com>
2021-01-22 18:05:30 +0530

    DEV-12064:Automation for microsites client hq toggle

---
+++ cypress/integration/microsites/clientHQMicrosite.js
@@ -0,0 +1,32 @@
+import { createAuthoringInstance } from '../../support/pageObject.js';
+
+const authoring = createAuthoringInstance(); 
+
+describe('Microsites - Client HQs', function() {
+    it('When Microsites is toggled on, microsite builder navigation should be visible, and it should take you to microsites page', function() {
+        if(authoring.common.env.TEST_ENV !== 'prod'){ // No superuser access on prod 
+            authoring.common.login();
+            authoring.clientHQ.clientHQToggle(authoring.clientHQ.micrositesToggle, 'on');
+            authoring.microsites.visit()
+            cy.get(authoring.common.pageTitleLocator).should('contain', authoring.microsites.pageTitle);
+            //When Microsites is toggled off, you should see Microsites appearance settings tab
+            authoring.configurations.visit.appearances(); 
+            cy.get(authoring.configurations.appearances.secondaryNav, {timeout: 10000}).within(() => {
+                cy.get('a[href$="microsite-builder"]').should('exist');
+            })
+        }
+    })
+    it('When Microsites is toggled off, you should see pemission denied message when microsite builder is navigated', function() {
+        if(authoring.common.env.TEST_ENV !== 'prod'){
+            authoring.common.login();
+            authoring.clientHQ.clientHQToggle(authoring.clientHQ.micrositesToggle, 'off');
+            authoring.microsites.visit()
+            cy.contains('You don\'t have permission to view this page.').should('exist');
+            //When Microsites is toggled off, you should not see Microsites appearance settings tab
+            authoring.configurations.visit.appearances(); 
+            cy.get(authoring.configurations.appearances.secondaryNav).within(() => {
+                cy.get('a[href$="microsite-builder"]').should('not.exist');
+            })
+        }
+    })
+})
