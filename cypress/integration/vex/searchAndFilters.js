1816a179993d1d8aeb31b0c5d06b80be361e3daf
Your Name <you@example.com>
2021-08-23 14:47:33 +0530

    DEV-14202 review comments incorporated

--- cypress/integration/vex/searchAndFilters.js
+++ cypress/integration/vex/searchAndFilters.js
@@ -134,43 +134,11 @@ const landingPage2 = {
     blocks: [
         {
             type: "Session Group",
-            sessionGroup: sessionGroupA.name,
-            searchConfiguration: {
-                enableToggle: true,
-                searchButtonTitle: "Test Search",
-            },
-            topicFilter: {
-                overrideLabel: 'VEX Filter By Topics Here',
-                enableToggle: true
-            },
-            availabilityFilter: {
-                overrideLabel: 'VEX Filter By Availability Here',
-                enableToggle: true
-            },
-            funnelStageFilter: {
-                enableToggle: true,
-                overrideLabel: 'VEX Filter By Funnel Stage Here'
-            },
-            industryFilter: {
-                enableToggle: true,
-                overrideLabel: 'VEX Filter By Industry Here'
-            },
-            personaFilter: {
-                enableToggle: true,
-                overrideLabel: 'VEX Filter By Persona Here'
-            },
-            businessUnitFilter: {
-                enableToggle: true,
-                overrideLabel: 'VEX Filter By Business Unit Here'
-            },
-            languageFilter: {
-                enableToggle: true,
-                overrideLabel: 'VEX Filter By Language Here'
-            }
+            sessionGroup: sessionGroupA.name
         },
         {
             type: "Session Group",
-            sessionGroup: "All Sessions",
+            sessionGroup: "All Sessions"
         },
     ]
 }
@@ -189,6 +157,7 @@ const landingPage3 = {
         },
     ]
 }
+
 const searchAndFilterOptions =
     [
         {
@@ -354,7 +323,8 @@ const filterOptions2 =
         }
 
     ]
-describe("Add and configure Search and filters, verify, apply in landing page and verify search and filters are working in consumption page", () => {
+
+describe("VEX- Add and configure Search and filters, verify, apply in landing page and verify search and filters are working in consumption page for VEX", () => {
 
     it("Set up VEX if doesn't exist", () => {
         cy.request({ url: event.url, failOnStatusCode: false }).then((response) => {
@@ -382,7 +352,6 @@ describe("Add and configure Search and filters, verify, apply in landing page an
             }
         })
     })
-
     it("Filter configurations in VEX consumption side", () => {
         authoring.common.login()
         authoring.vex.visit();
@@ -397,7 +366,6 @@ describe("Add and configure Search and filters, verify, apply in landing page an
         authoring.vex.addAdvancedBlock(landingPage.blocks[0]) //Block level filter configuration
         cy.contains('button', 'Save').click();
         cy.visit(event.url)
-        cy.wait(5000)
         consumption.vex.verifyLandingPageBlock(landingPage.blocks[0]) // consumption verification for block level search and filters
         //Verify VEX authoring and consumption side when Landing page filter configuration made from  VEX- "Search & Filter Tab"
         //Verify if the block has all sessions visible from the event when "￼All Session￼"  has been configured in the dropdown in the landing page builder block
@@ -407,7 +375,7 @@ describe("Add and configure Search and filters, verify, apply in landing page an
         authoring.vex.saveSearchAndFiltersSettings();
         authoring.vex.goToLandingPage()
         authoring.vex.goToPageEditor(landingPage.name)
-        cy.wait(2000)
+        cy.wait(4000)
         cy.get(authoring.vex.pages.sessionCardTitle).eq(0).click({force:true})
         authoring.vex.addAdvancedBlock(landingPage.blocks[1]) //Adding a new block(All Sessions)to verify the configurations
         cy.contains('button', 'Save').click();
@@ -498,7 +466,7 @@ describe("Add and configure Search and filters, verify, apply in landing page an
 
     })
 
-    it.only("Update landing page block filter capabilities to be a multi-select format", () => {
+    it("Update landing page block filter capabilities to be a multi-select format", () => {
         authoring.common.login()
         authoring.vex.visit();
         authoring.vex.goToEventConfig(event.name)
@@ -511,6 +479,7 @@ describe("Add and configure Search and filters, verify, apply in landing page an
         cy.visit(event.url)
         //Validate if sessions are filtered based on the multi-selection in a virtual event landing page
         //In Group A Block : Multiple Topics Filter applied
+        cy.wait(4000)
         cy.contains(consumption.vex.sessionGroup, landingPage.blocks[1].sessionGroup).within(() => {
             cy.get(consumption.vex.topicFilter).click()
             cy.wait(1000)
@@ -582,180 +551,6 @@ describe("Add and configure Search and filters, verify, apply in landing page an
         //Verify the sessions after applying the search filter
         cy.contains(consumption.vex.sessionCardTitle, publicSession[0].name).should("exist")
 
-
-        cy.get(authoring.vex.pages.sessionCardTitle).eq(0).click()
-        authoring.vex.addAdvancedBlock(landingPage.blocks[1]) //Adding a new block(All Sessions)to verify the configurations
-        cy.contains('button', 'Save').click();
-        //Verify the Search & Filter tab configurations for VEX
-        //Verify if the default filter and search button, placeholder name  is pulled from the language configuration section if no label override is provided at the block level.[This functionality is separately covered in vexLanguage.js file]
-        //Search and filter tab configurations in VEX Authoring Side
-        cy.contains(authoring.vex.pages.sessionGroupRow, landingPage.blocks[1].sessionGroup).within(() => {
-            //language override
-            cy.contains("option", languageOverrideForFilters.availability).should("exist")
-            cy.contains("option", languageOverrideForFilters.topics).should("exist")
-            cy.contains("button", languageOverrideForFilters.searchButton).should("exist")
-            cy.get('input').should("have.attr", "placeholder", languageOverrideForFilters.searchInput)
-            //filter configurations value
-            cy.get(authoring.vex.topicFilter).should("exist")
-            cy.get(authoring.vex.availabilityFilter).should("exist")
-            cy.get(authoring.vex.funnelStageFilter).should("exist")
-            cy.get(authoring.vex.industryFilter).should("exist")
-            cy.get(authoring.vex.personaFilter).should("exist")
-            cy.get(authoring.vex.businessUnitFilter).should("exist")
-            cy.get(authoring.vex.languageFilter).should("exist")
-            cy.get(authoring.vex.searchInputText).should("exist")
-            cy.get(authoring.vex.searchButton).should("exist")
-        })
-        //Search and filter tab configurations in VEX Consumption Side
-        cy.visit(event.url)
-        cy.contains(consumption.vex.sessionGroup, landingPage.blocks[1].sessionGroup).within(() => {
-            //language override
-            cy.contains("div", languageOverrideForFilters.availability).should("exist")
-            cy.contains("div", languageOverrideForFilters.topics).should("exist")
-            cy.contains("div", languageOverrideForFilters.searchButton).should("exist")
-            cy.get('input:visible').should("have.attr", "placeholder", languageOverrideForFilters.searchInput)
-            //filter configurations value
-            cy.get(consumption.vex.topicFilter).should("exist")
-            cy.get(consumption.vex.availabilityFilter).should("exist")
-            cy.get(consumption.vex.funnelStageFilter).should("exist")
-            cy.get(consumption.vex.industryFilter).should("exist")
-            cy.get(consumption.vex.personaFilter).should("exist")
-            cy.get(consumption.vex.businessUnitFilter).should("exist")
-            cy.get(consumption.vex.languageFilter).should("exist")
-            cy.get(consumption.vex.searchInput).should("exist")
-            cy.get(consumption.vex.searchButton).should("exist")
-        })
-        //Verify content tags filter dropdown in VEX consumption, when there are no values configured in "Search & Filter Tab"
-        //[Note : Filter dropdown should display with no value in this scenario]
-        authoring.vex.visit();
-        authoring.vex.goToEventConfig(event.name)
-        authoring.vex.removeSearchAndFilterValues(searchAndFilterOptions);
-        authoring.vex.saveSearchAndFiltersSettings();
-        authoring.vex.goToLandingPage()
-        authoring.vex.goToPageEditor(landingPage.name)
-        cy.visit(event.url)
-        //Verify Topic Filter dropdown 
-        cy.contains(consumption.vex.sessionGroup, landingPage.blocks[1].sessionGroup).within(() => {
-            cy.get(consumption.vex.topicFilter).click()
-            cy.wait(2000)
-        })
-        cy.get(consumption.vex.checkbox).should('have.length', 1)
-        //Verify Funnel stage Filter dropdown 
-        cy.contains(consumption.vex.sessionGroup, landingPage.blocks[1].sessionGroup).within(() => {
-            cy.get(consumption.vex.funnelStageFilter).click()
-            cy.wait(2000)
-        })
-        cy.get(consumption.vex.checkbox).should('have.length', 1)
-        //Verify Industry Filter dropdown 
-        cy.contains(consumption.vex.sessionGroup, landingPage.blocks[1].sessionGroup).within(() => {
-            cy.get(consumption.vex.industryFilter).click()
-            cy.wait(2000)
-        })
-        cy.get(consumption.vex.checkbox).should('have.length', 1)
-        //Verify Persona Filter dropdown 
-        cy.contains(consumption.vex.sessionGroup, landingPage.blocks[1].sessionGroup).within(() => {
-            cy.get(consumption.vex.personaFilter).click()
-            cy.wait(2000)
-        })
-        cy.get(consumption.vex.checkbox).should('have.length', 1)
-        //Verify Business Units Filter dropdown 
-        cy.contains(consumption.vex.sessionGroup, landingPage.blocks[1].sessionGroup).within(() => {
-            cy.get(consumption.vex.businessUnitFilter).click()
-            cy.wait(2000)
-        })
-        cy.get(consumption.vex.checkbox).should('have.length', 1)
-        //Verify Language Filter dropdown 
-        cy.contains(consumption.vex.sessionGroup, landingPage.blocks[1].sessionGroup).within(() => {
-            cy.get(consumption.vex.languageFilter).click()
-            cy.wait(2000)
-        })
-        cy.get(consumption.vex.checkbox).should('have.length', 1)
-
-    })
-
-    it.only("Update landing page block filter capabilities to be a multi-select format", () => {
-        authoring.common.login()
-        authoring.vex.visit();
-        authoring.vex.goToEventConfig(event.name)
-        authoring.vex.goToSessionList()
-        authoring.vex.configureSessionTagging(singleSessionTagging)
-        authoring.vex.configureSessionTagging(multipleSessionTagging)
-        authoring.vex.addSearchAndFilterOptions(searchAndFilterOptions)
-        authoring.vex.saveSearchAndFiltersSettings();
-        //Visit Consumption page
-        cy.visit(event.url)
-        //Validate if sessions are filtered based on the multi-selection in a virtual event landing page
-        //In Group A Block : Multiple Topics Filter applied
-        cy.contains(consumption.vex.sessionGroup, landingPage.blocks[1].sessionGroup).within(() => {
-            cy.get(consumption.vex.topicFilter).click()
-            cy.wait(1000)
-
-        })
-        cy.contains('div', multipleSessionTagging.topics[0], { timeout: 1000 }).parent().should('be.visible').click()
-        cy.contains('div', multipleSessionTagging.topics[1], { timeout: 1000 }).parent().should('be.visible').click()
-        cy.get(consumption.vex.topicFilter).should('have.contain', multipleSessionTagging.topics[0])
-        cy.get(consumption.vex.topicFilter).should('have.contain', multipleSessionTagging.topics[1])
-        cy.get(consumption.vex.cancelFilterbox).click()
-        //Multiple Funnel Stages filter applied
-        cy.contains(consumption.vex.sessionGroup, landingPage.blocks[1].sessionGroup).within(() => {
-            cy.get(consumption.vex.funnelStageFilter).click()
-            cy.wait(1000)
-        })
-        cy.contains('div', multipleSessionTagging.funnelStages[0], { timeout: 1000 }).parent().should('be.visible').click()
-        cy.contains('div', multipleSessionTagging.funnelStages[1], { timeout: 1000 }).parent().should('be.visible').click()
-        cy.get(consumption.vex.funnelStageFilter).should('have.contain', multipleSessionTagging.funnelStages[0])
-        cy.get(consumption.vex.funnelStageFilter).should('have.contain', multipleSessionTagging.funnelStages[1])
-        cy.get(consumption.vex.cancelFilterbox).click()
-        //Verify the sessions after applying the filters
-        cy.contains(consumption.vex.sessionCardTitle, publicSession[0].name).should("exist")
-        cy.contains(consumption.vex.sessionCardTitle, publicSession[1].name).should("exist")
-        //Persona filter applied
-        cy.contains(consumption.vex.sessionGroup, landingPage.blocks[1].sessionGroup).within(() => {
-            cy.get(consumption.vex.personaFilter).click()
-            cy.wait(1000)
-        })
-        cy.contains('div', multipleSessionTagging.personas[1], { timeout: 1000 }).parent().should('be.visible').click()
-        cy.get(consumption.vex.personaFilter).should('have.contain', multipleSessionTagging.personas[1])
-        cy.get(consumption.vex.cancelFilterbox).click()
-        //Verify the sessions after applying the filters
-        cy.contains(consumption.vex.sessionCardTitle, publicSession[0].name).should("exist")
-        cy.contains(consumption.vex.sessionCardTitle, publicSession[1].name).should("not.exist")
-        //Verify if the Select All(a checkbox on top) option is available in the filter dropdown.(checking for any single filter)
-        cy.reload()
-        cy.contains(consumption.vex.sessionGroup, landingPage.blocks[1].sessionGroup).within(() => {
-            cy.get(consumption.vex.topicFilter).click()
-            cy.wait(1000)
-        })
-        cy.get(consumption.vex.filterBoxheader).within(() => {
-            cy.get(consumption.vex.selectAllFilterCheckbox).click()
-        })
-        cy.get(consumption.vex.cancelFilterbox).click()
-        //Verify the "Select All" filter result
-        cy.contains(consumption.vex.sessionCardTitle, publicSession[0].name).should("exist")
-        cy.contains(consumption.vex.sessionCardTitle, publicSession[1].name).should("exist")
-        //Funnel stage & Availability filter should not show a search bar
-        //Availability filter 
-        cy.contains(consumption.vex.sessionGroup, landingPage.blocks[1].sessionGroup).within(() => {
-            cy.get(consumption.vex.availabilityFilter).click()
-            cy.wait(1000)
-        })
-        cy.get(consumption.vex.filterSearchBox).should("not.exist")
-        cy.get(consumption.vex.cancelFilterbox).click()
-        //Funnel stage filter 
-        cy.contains(consumption.vex.sessionGroup, landingPage.blocks[1].sessionGroup).within(() => {
-            cy.get(consumption.vex.funnelStageFilter).click()
-            cy.wait(1000)
-        })
-        cy.get(consumption.vex.filterSearchBox).should("not.exist")
-        cy.get(consumption.vex.cancelFilterbox).click()
-        //Verify search functionality for VEX sessions
-        cy.reload()
-        cy.contains(consumption.vex.sessionGroup, landingPage.blocks[1].sessionGroup).within(() => {
-            cy.get(consumption.vex.searchInputField).type(publicSession[0].name)
-            cy.get(consumption.vex.searchFilter).click()
-        })
-        //Verify the sessions after applying the search filter
-        cy.contains(consumption.vex.sessionCardTitle, publicSession[0].name).should("exist")
     })
 
     it("Verify applied landing page block filters as query strings in URL", () => {
