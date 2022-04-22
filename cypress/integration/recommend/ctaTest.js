69d5b64801250455c112e141caaf1c266830018a
Kinjal <kinjal.solanki@pathfactory.com>
2022-04-22 15:02:55 -0400

    added

--- cypress/integration/recommend/ctaTest.js
+++ cypress/integration/recommend/ctaTest.js
@@ -2,12 +2,15 @@ import { createAuthoringInstance, createConsumptionInstance } from '../../suppor
 
 const authoring = createAuthoringInstance({org: "automation-recommend", tld: "lookbookhq"})
 const consumption = createConsumptionInstance({org: 'automation-recommend', tld: 'lookbookhq'})
+const contents = authoring.common.env.orgs["automation-recommend"].resources
+const webContent = contents["Website Common Resource"]
 
 const recommend = {
     name: 'ctaTest.js',
     slug: 'ctatest-js',
+    contents: [webContent.title],
     get url(){
-        return `${authoring.common.baseUrl}/${this.slug}`
+        return `${authoring.common.baseUrl}/${this.slug}/commonresource`
     },
 
 }
@@ -33,7 +36,7 @@ const ctas =[
         },
         {
             ctaNumber: "CTA 3",
-            ctaName: "dynamicFieldMergeRecommend.js",
+            ctaName: "googleCTA",
             location: "Before assets",
             buttonColor: "#04977d",
             fontColor: "#483d1e",
@@ -43,12 +46,14 @@ const ctas =[
     ]
 
 describe("Explore CTA buttons", () => {
-it.only("Add and delete CTA buttons", () => {
+it("Add and delete CTA buttons", () => {
     authoring.common.login()
     authoring.recommend.visit()
     //authoring.recommend.goToTrack(recommend.name)
     authoring.recommend.deleteTrack(recommend.name)
     authoring.recommend.addTrack(recommend)
+    authoring.recommend.configure(recommend)
+
 
     // turn CTA toggle ON
     authoring.common.toggle(authoring.recommend.pageSidebar.sidebarToggle, 'ON')    
@@ -56,8 +61,88 @@ it.only("Add and delete CTA buttons", () => {
 
     authoring.recommend.configureSidebarwithCtas(cta)
 
-    
     })
 
-})
+    cy.visit(recommend.url) 
+    cy.contains("button", "formcta").should("exist")
+        //verify specific alignment selected gets applied to the cells
+     cy.get('[class*="pf-multi-cta pf-cta-1"]').should("have.css", "background-color", "rgb(4, 151, 125)")
+                                                                       .should("have.css", "color", "rgb(72, 61, 30)")
+                    
+    cy.window().then((win) => {
+    cy.get('button[class*="pf-multi-cta pf-cta-1"]').then(($el) => {
+    expect(win.getComputedStyle($el[0]).width).to.eq('100px')
+                                                                    
+       })
+  })
+
+  cy.contains("a", "linkcta").should("exist")
+       cy.get('a[class*="pf-multi-cta pf-cta-2"]').should("have.css", "background-color", "rgb(4, 151, 125)")
+                                                  .should("have.css", "color", "rgb(72, 61, 30)")
+     cy.window().then((win) => {
+     cy.get('a[class*="pf-multi-cta pf-cta-2"]').then(($el) => {
+     expect(win.getComputedStyle($el[0]).width).to.eq('100px')
+                                                
+             })
+        })
+
+    cy.contains("a", "googleCtaLink").should("exist")
+       cy.get('a[class*="pf-multi-cta pf-cta-3"]').should("have.css", "background-color", "rgb(4, 151, 125)")
+                                                  .should("have.css", "color", "rgb(72, 61, 30)")
+      cy.window().then((win) => {
+     cy.get('a[class*="pf-multi-cta pf-cta-3"]').then(($el) => {
+    expect(win.getComputedStyle($el[0]).width).to.eq('119.945px')
+                                                
+                     })
+          })
+
+      })
+
+      it("Add and delete CTA buttons", () => {
+        authoring.common.login()
+        authoring.recommend.visit()
+        authoring.recommend.goToTrack(recommend.name)
+    
+        // turn CTA toggle ON
+        authoring.common.toggle(authoring.recommend.pageSidebar.topicSidebarToggle, 'ON')    
+        ctas.forEach((cta)=>{
+    
+        authoring.recommend.configureTopicSidebarwithCtas(cta)
+    
+        })
+    
+        cy.visit(recommend.url) 
+        cy.contains("button", "formcta").should("exist")
+            //verify specific alignment selected gets applied to the cells
+         cy.get('[class*="pf-multi-cta pf-cta-1"]').should("have.css", "background-color", "rgb(4, 151, 125)")
+                                                                           .should("have.css", "color", "rgb(72, 61, 30)")
+                        
+        cy.window().then((win) => {
+        cy.get('button[class*="pf-multi-cta pf-cta-1"]').then(($el) => {
+        expect(win.getComputedStyle($el[0]).width).to.eq('100px')
+                                                                        
+           })
+      })
+    
+      cy.contains("a", "linkcta").should("exist")
+           cy.get('a[class*="pf-multi-cta pf-cta-2"]').should("have.css", "background-color", "rgb(4, 151, 125)")
+                                                      .should("have.css", "color", "rgb(72, 61, 30)")
+         cy.window().then((win) => {
+         cy.get('a[class*="pf-multi-cta pf-cta-2"]').then(($el) => {
+         expect(win.getComputedStyle($el[0]).width).to.eq('100px')
+                                                    
+                 })
+            })
+    
+        cy.contains("a", "googleCtaLink").should("exist")
+           cy.get('a[class*="pf-multi-cta pf-cta-3"]').should("have.css", "background-color", "rgb(4, 151, 125)")
+                                                      .should("have.css", "color", "rgb(72, 61, 30)")
+          cy.window().then((win) => {
+         cy.get('a[class*="pf-multi-cta pf-cta-3"]').then(($el) => {
+        expect(win.getComputedStyle($el[0]).width).to.eq('119.945px')
+                                                    
+                         })
+              })
+    
+          })
 })
--- cypress/support/authoringClasses/Recommend.js
+++ cypress/support/authoringClasses/Recommend.js
@@ -425,6 +425,38 @@ export class Recommend extends Common {
         }
     }
 
+    configureTopicSidebarwithCtas(config){
+        const ctaNumber = config.ctaNumber
+        const ctaName = config.ctaName
+        const location = config.location
+        const buttonColor = config.buttonColor
+        const fontColor = config.fontColor
+        const addcta = config.addcta
+
+       cy.get(this.pageSidebar.topicSidebarToggle).parents().eq(1).within(() => {
+            cy.contains("label", ctaNumber).siblings("span").click()
+        })
+            cy.get(this.popover).within(()=>{
+            if(Cypress.$(this.clearValueIcon).length > 0){
+                cy.get(this.clearValueIcon).click()
+            }
+            cy.get(this.dropdown.box).eq(0).click()
+            cy.get(this.dropdown.input).eq(0).type(ctaName + "\n", {force: true})
+            cy.get(this.dropdown.box).eq(1).click()
+            cy.get(this.dropdown.input).eq(1).type(location + "\n", {force: true})
+            cy.get('#buttonColor').type(buttonColor)
+            cy.wait(100)
+            cy.get('#fontColor').type(fontColor)
+            cy.wait(200)
+            cy.contains('button', 'Update').click()
+        })
+
+        if(addcta){
+            cy.contains('div', "+ Add CTA").click({force: true})
+            cy.wait(2000)
+        }
+    }
+
     configureExit(exitOptions, verify){
         const { delay } = exitOptions
 
